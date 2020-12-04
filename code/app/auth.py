6dbb11bdeec98f80a52cf9b2d583b021cc58b23f
Onur Erhan <contact@onurerhan.com>
2020-12-04 23:22:46 +0300

    added user name to user profile added creation of achievement progress on login

--- code/app/auth.py
+++ code/app/auth.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from rest_framework import authentication
 from firebase_admin import auth
 
-from app.models import Profile, Achievements
+from app.models import Profile, Achievements, AchievementProgress
 
 
 class FirebaseAuthentication(authentication.BaseAuthentication):
@@ -17,6 +17,9 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
             decoded_token = auth.verify_id_token(token)
             uid = decoded_token['uid']
             email = decoded_token['email']
+            username = ''
+            if 'name' in decoded_token:
+                username = decoded_token['name']
         except Exception as e:
             print(e)
             return None
@@ -25,8 +28,13 @@ class FirebaseAuthentication(authentication.BaseAuthentication):
             user, result = User.objects.get_or_create(username=uid, email=email)
             user.last_login = timezone.now()
             user.save()
-            Profile.objects.get_or_create(owner=user)
-            Achievements.objects.get_or_create(owner=user)
+            profile, _ = Profile.objects.get_or_create(owner=user)
+            if username != '':
+                profile.shown_username = username
+            profile.save()
+            all_achievements = Achievements.objects.all()
+            for achievement in all_achievements:
+                AchievementProgress.objects.get_or_create(achievement=achievement, owner=user)
             return [user, token]
 
         except Exception as e:
